31a554addb823e647ad3a70ed7b59ca4c9dbc77d
abourass <abourassa@assetval.com>
2020-09-20 14:59:33 -0600

    :art: Adding some design to the homepage

--- sample/app.ts
+++ sample/app.ts
@@ -5,33 +5,29 @@ import nest from './tools/nest.js';
 
 const theme = new Style('');
 
-create('button', '#prisonHook').addClass(['button', 'is-danger']).text('Generate Prison').on('click', ()=> {
+create('button', '#prisonHook').addClass(['button', 'is-warning']).text('Generate Prison').on('click', ()=> {
   const prison = new Prison('Example Prison');
-  console.table(prison)
 
-  nest('div', '#generateOutput', (tiles) => {
-    nest('div', tiles, (mainTile => {
-      nest('article', mainTile, (article => {
-        create('p', article).addClass('title').text('Example Prison');
-        create('p', article).addClass('subtitle').text(prison.type);
-        nest('div', article, (content) => {
-          nest('div', content, (titles) => {
-            nest('p', titles, (guardSection) => {
-              create('span', guardSection).text('Guards: ').set({style: 'font-weight: 600'})
-              create('span', guardSection).text(prison.guards)
-            });
-            nest('p', titles, (convictsSection) => {
-              create('span', convictsSection).text('Convicts: ').set({style: 'font-weight: 600'})
-              create('span', convictsSection).text(prison.guards)
-            })
-            nest('p', titles, (helpSection) => {
-              create('span', helpSection).text('Help: ').set({style: 'font-weight: 600'})
-              create('span', helpSection).text(prison.guards)
-            });
-          }).set({style: 'display: flex; flex-direction: column;'}).addClass(['column'])
-        }).addClass(['content', 'columns'])
-      })).addClass(['tile', 'is-child', 'notification'])
-    })).addClass(['tile', 'is-parent'])
-  }).addClass(['tile', 'is-ancestor'])
-
+  nest('div', '#generateOutput', (mainTile => {
+    nest('article', mainTile, (article => {
+      create('p', article).addClass('title').text('Example Prison');
+      create('p', article).addClass('subtitle').text(prison.type);
+      nest('div', article, (content) => {
+        nest('div', content, (titles) => {
+          nest('p', titles, (guardSection) => {
+            create('span', guardSection).text('Guards: ').set({style: 'font-weight: 600'})
+            create('span', guardSection).text(prison.guards)
+          });
+          nest('p', titles, (convictsSection) => {
+            create('span', convictsSection).text('Convicts: ').set({style: 'font-weight: 600'})
+            create('span', convictsSection).text(prison.convicts)
+          })
+          nest('p', titles, (helpSection) => {
+            create('span', helpSection).text('Help: ').set({style: 'font-weight: 600'})
+            create('span', helpSection).text(prison.help)
+          });
+        }).set({style: 'display: flex; flex-direction: column;'}).addClass(['column'])
+      }).addClass(['content', 'columns'])
+    })).addClass(['tile', 'is-child', 'notification', 'is-warning'])
+  })).addClass(['tile', 'is-parent'])
 })
