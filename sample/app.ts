0caa743fbb2b21f6162705382bb0a7b4d60d9142
abourass <abourassa@assetval.com>
2020-09-20 12:52:17 -0600

    :sparkles: Creating a sample site

--- sample/app.ts
+++ sample/app.ts
@@ -1,5 +1,11 @@
 import create from './tools/create.js';
 import Prison from '../src/buildings/Prison.js';
+import Style from './tools/style.js';
+
+// @ts-ignore
+import Bulma from 'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css';
+
+const theme = new Style(Bulma);
 
 create('button', 'body').text('Click me').on('click', ()=> {
   console.log(new Prison('Silvie'))
