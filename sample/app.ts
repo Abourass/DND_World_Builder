6627c38efceee75cd5ae96ac0992f4cd76ec6e4d
abourass <abourassa@assetval.com>
2020-09-20 12:56:10 -0600

    :sparkles: Trying to add Bulma as a style

--- sample/app.ts
+++ sample/app.ts
@@ -1,9 +1,7 @@
 import create from './tools/create.js';
 import Prison from '../src/buildings/Prison.js';
 import Style from './tools/style.js';
-
-// @ts-ignore
-import Bulma from 'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css';
+import Bulma from './css/Bulma.js';
 
 const theme = new Style(Bulma);
 
